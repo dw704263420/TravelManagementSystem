6f2bec2fb59a803c22fd310517ea9072ed602b48
倪俊伟 <7810509+ni_junwei@user.noreply.gitee.com>
2021-05-03 10:29:25 +0800

    删除文件 src

--- src/main/java/com/kuang/service/BannerService.java
+++
@@ -1,9 +0,0 @@
-package com.kuang.service;
-
-import com.kuang.pojo.Banner;
-
-import java.util.List;
-
-public interface BannerService {
-    public List<Banner> findAllBanner();
-}
--- src/main/java/com/kuang/service/CategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.kuang.service;
-
-import com.kuang.pojo.Category;
-
-import java.util.List;
-
-public interface CategoryService {
-    public List<Category> findAll() throws Exception;
-}
